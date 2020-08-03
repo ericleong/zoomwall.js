2bf5f13c5f9cb29c3e9a61640619289ea40d24e6
Eric Leong <me@ericleong.me>
2020-08-03 03:49:38 -0400

    Add lightbox test with no layout checks.

--- zoomwall.test.js
+++ zoomwall.test.js
@@ -15,3 +15,22 @@ test('expand rows to fill width of parent', () => {
   expect(gallery.children[1].style.width).toBe('50%');
   expect(gallery.children[1].style.height).toBe('auto');
 });
+
+test('clicking image transitions to lightbox', () => {
+
+  document.body.innerHTML = 
+  '<div id="gallery" class="zoomwall" style="width: 1024px, height: 768px">' + 
+  '  <img src="01_lowres.jpg" data-highres="01_highres.jpg" width="250" height="167" style="width: 250px, height: 167px"/>' +
+  '  <img src="02_lowres.jpg" data-highres="02_highres.jpg" width="250" height="167" style="width: 250px, height: 167px"/>' +
+  '</div>';
+
+  zoomwall.create(document.getElementById('gallery'));
+
+  const gallery = document.getElementById('gallery');
+  const selected = gallery.children[1];
+  selected.click();
+
+  expect(gallery.classList).toContain('lightbox');
+  expect(selected.classList).toContain('active');
+  expect(selected.src).toBe(`${window.location}02_highres.jpg`);
+});
