68a0016111be30a778b00759885d952bb04c0ff0
Eric Leong <me@ericleong.me>
2020-08-11 17:37:40 -0400

    Remove test files.

--- test/docs/assets/js/search.js
+++
@@ -1,3 +0,0 @@
-var typedoc = typedoc || {};
-            typedoc.search = typedoc.search || {};
-            typedoc.search.data = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"create","url":"globals.html#create","classes":"tsd-kind-function"},{"id":1,"kind":64,"name":"keys","url":"globals.html#keys","classes":"tsd-kind-function"}]};
