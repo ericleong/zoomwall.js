82a0e0915b3d730838ea00fc6640be06ce30c3fc
ericleong <ericleong@users.noreply.github.com>
2020-08-13 03:45:29 +0000

    deploy: f547c478e8c8bfd84603fad9c0b9484ac2488008

---
+++ docs/docs/assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"create","url":"globals.html#create","classes":"tsd-kind-function"}]};
---
+++ lib/lib/zoomwall.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Create a gallery with the provided HTMLElement.
+ *
+ * @param blocks contains the images that belong to the gallery
+ * @param enableKeys enables keyboard navigation
+ */
+export declare function create(blocks: HTMLElement, enableKeys?: boolean): void;
